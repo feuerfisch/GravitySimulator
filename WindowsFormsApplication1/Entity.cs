d8999686cc752563c5af42c77191fc6fb2796c45
Fischer <Fischer@FISCHER>
2016-11-08 20:55:26 +0100

    rebased gravity handling, moved code from Entity.cs to World.cs

--- WindowsFormsApplication1/Entity.cs
+++ WindowsFormsApplication1/Entity.cs
@@ -8,15 +8,15 @@ namespace WindowsFormsApplication1
 {
     class Entity
     {
-        public double BESCHLEUNIGUNGSKONSTANTE = 0.00000001;
-        string name { get; set; }
+        public string name { get; set; }
         public double x { get; set; }
         public double y { get; set; }
         public double z { get; set; }
-        double vx { get; set; }
-        double vy { get; set; }
-        double vz { get; set; }
-        double masse { get; set; }
+        public double vx { get; set; }
+        public double vy { get; set; }
+        public double vz { get; set; }
+        public double masse { get; set; }
+
         public Entity(string _name, double _x, double _y, double _z, double _vx, double _vy, double _vz, double _masse)
         {
             name = _name;
@@ -27,37 +27,6 @@ namespace WindowsFormsApplication1
             vy = _vy;
             vz = _vz;
             masse = _masse;
-        }
-        public void calcv(List<Entity> entityList)
-        {
-            foreach (Entity Element in entityList)
-            {
-                if (Element != this)
-                {
-                    double dx = (this.x - Element.x);
-                    double dy = (this.y - Element.y);
-                    double dz = (this.z - Element.z);
-                    double dx3abs = Math.Round(System.Math.Abs((dx * dx * dx)), 5);
-                    double dy3abs = Math.Round(System.Math.Abs((dy * dy * dy)), 5);
-                    double dz3abs = Math.Round(System.Math.Abs((dz * dz * dz)), 5);
-                    double ax = 0;
-                    double ay = 0;
-                    double az = 0;
-                    double distanz = Math.Pow((dx3abs + dy3abs + dz3abs), 1.0 / 3.0);
-                    if (distanz != 0) { ax = -BESCHLEUNIGUNGSKONSTANTE * Element.masse * dx / distanz; }
-                    if (distanz != 0) { ay = -BESCHLEUNIGUNGSKONSTANTE * Element.masse * dy / distanz; }
-                    if (distanz != 0) { az = -BESCHLEUNIGUNGSKONSTANTE * Element.masse * dz / distanz; }
-                    vx = vx + ax;
-                    vy = vy + ay;
-                    vz = vz + az;
-                }
-            }
-        }
-        public void calcpos()
-        {
-            x = x + vx;
-            y = y + vy;
-            z = z + vz;
-        }
+        }  
     }
 }
--- WindowsFormsApplication1/World.cs
+++ WindowsFormsApplication1/World.cs
@@ -8,24 +8,47 @@ namespace WindowsFormsApplication1
 {
     class World
     {
+        public double BESCHLEUNIGUNGSKONSTANTE = 0.00000001;
         public List<Entity> entityList { get; set; }
         public World()
         {
-        }
-        public void calcpos()
-        {
-            calcv();
-            foreach (Entity Element in entityList)
-            {
-                Element.calcpos();
-            }
         }
 
         void calcv()
         {
             foreach (Entity Element in entityList)
             {
-                Element.calcv(entityList);
+                foreach (Entity Iterator in entityList)
+                    if (Iterator != Element)
+                {
+                    double dx = (Element.x - Iterator.x);
+                    double dy = (Element.y - Iterator.y);
+                    double dz = (Element.z - Iterator.z);
+                    double dx3abs = Math.Round(System.Math.Abs((dx * dx * dx)), 5);
+                    double dy3abs = Math.Round(System.Math.Abs((dy * dy * dy)), 5);
+                    double dz3abs = Math.Round(System.Math.Abs((dz * dz * dz)), 5);
+                    double ax = 0;
+                    double ay = 0;
+                    double az = 0;
+                    double distanz = Math.Pow((dx3abs + dy3abs + dz3abs), 1.0 / 3.0);
+                    if (distanz != 0) { ax = -BESCHLEUNIGUNGSKONSTANTE * Element.masse * dx / distanz; }
+                    if (distanz != 0) { ay = -BESCHLEUNIGUNGSKONSTANTE * Element.masse * dy / distanz; }
+                    if (distanz != 0) { az = -BESCHLEUNIGUNGSKONSTANTE * Element.masse * dz / distanz; }
+                        Element.vx = Element.vx + ax;
+                        Element.vy = Element.vy + ay;
+                        Element.vz = Element.vz + az;
+                }
+            }
+        }
+
+        public void calcpos()
+        {
+            calcv();
+            foreach (Entity Element in entityList)
+            {
+                Element.x = Element.x + Element.vx;
+                Element.y = Element.y + Element.vy;
+                Element.z = Element.z + Element.vz;
             }
         }
     }
